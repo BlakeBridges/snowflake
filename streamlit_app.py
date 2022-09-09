b186b78c093ff865e72b2258d92d35dbe85fd0b5
Blake Bridges <71102189+BlakeBridges@users.noreply.github.com>
2022-09-09 15:34:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
-streamlit.dataframe(fruit_list)
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -12,3 +12,6 @@ streamlit.text('Hard-Boiled Free-Range Egg🥣 🥗 🐔 🥑🍞')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.multiselect("Pick some", list(fruit_list.index))
+streamlit.dataframe(fruit_list)
