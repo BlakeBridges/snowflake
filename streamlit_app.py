20f8f80f76076627bc39c4abdbb3fda7f09ce0bb
Blake Bridges <71102189+BlakeBridges@users.noreply.github.com>
2022-09-09 15:27:07 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
-streamlit.title('Hello all')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg🥣 🥗 🐔 🥑🍞')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
