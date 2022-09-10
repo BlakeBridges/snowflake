c1b717c52a059e6c0dafba68f6c6c6ca1d664d0c
Blake Bridges <71102189+BlakeBridges@users.noreply.github.com>
2022-09-10 01:28:35 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('FruityV')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #Uses pandas to normalize the json 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
