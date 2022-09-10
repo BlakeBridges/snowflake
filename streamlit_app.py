7397ef5d4ece35e245fed60c58d9ab7d78799d61
Blake Bridges <71102189+BlakeBridges@users.noreply.github.com>
2022-09-10 03:01:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("List contains:")
 streamlit.dataframe(my_data_rows)
 
 streamlit.header("Add fruit?")
-fruit_choice = streamlit.text_input('What fruit would you like add?','Kiwi')
-my_cur.execute(f"insert into fruit_load_list values('{fruit_choice}');")
+fruit_choice_insert = streamlit.text_input('What fruit would you like add?','Kiwi')
+my_cur.execute(f"insert into fruit_load_list values('{fruit_choice_insert}');")
